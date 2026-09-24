aec9f60bcc525606babdf9e0f7f2a80841cdfba2
Pedro Henrique Borges Silva <engpedro.run@gmail.com>
2026-09-24 00:14:53 -0300

    Implementa cadastro e notificação dos observadores
    Adiciona no telefone a lista de observadores e a lógica de aviso.

--- PhoneModel.java
+++ PhoneModel.java
@@ -3,9 +3,11 @@ import java.util.List;
 
 public class PhoneModel {
     private List<Integer> digits = new ArrayList<>();
+    private List<PhoneObserver> observers = new ArrayList<>();
 
     public void addDigit(int newDigit) {
         digits.add(newDigit);
+        notifyObservers();
     }
 
     public List<Integer> getDigits() { return digits; }
@@ -14,5 +16,15 @@ public class PhoneModel {
         String number = "";
         for (Integer digit : digits) number += digit;
         return number;
+    }
+
+    public void addObserver(PhoneObserver observer) {
+        observers.add(observer);
+    }
+
+    private void notifyObservers() {
+        for (PhoneObserver observer : observers) {
+            observer.update(this);
+        }
     }
 }
