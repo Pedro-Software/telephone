e9422f727bccdd1d54dc704933323b8a404a87c2
Pedro Henrique Borges Silva <engpedro.run@gmail.com>
2026-09-24 00:15:14 -0300

    Adiciona observadores da tela para exibir informações
    A Screen cria os dois observadores solicitados.

--- Screen.java
+++ Screen.java
@@ -1,4 +1,12 @@
 public class Screen {
     public Screen(PhoneModel model) {
+        model.addObserver(phone -> {
+            int last = phone.getDigits().get(phone.getDigits().size() - 1);
+            System.out.println(last);
+        });
+
+        model.addObserver(phone -> {
+            System.out.println("Agora discando " + phone.getNumber() + "...");
+        });
     }
 }
